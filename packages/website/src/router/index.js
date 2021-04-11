080132a4cac300d0b285477b9a9b7773ba108141
allenhush <allenshhu@outlook.com>
2021-04-11 10:54:53 +0800

    fix: changed the router mode to hash

--- packages/website/src/router/index.js
+++ packages/website/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import generateRutersByDir from './router.js'
 import Home from '../views/Home.vue'
 import Test from '../views/Test.vue'
@@ -32,7 +32,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
