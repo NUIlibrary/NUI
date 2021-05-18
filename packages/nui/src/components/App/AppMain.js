0c42ab693cc235ec3bc80b7036292271f18ae981
AllenHUSH <allenshhu@outlook.com>
2021-05-19 01:57:21 +0800

    fix: app-bar height

--- packages/nui/src/components/App/AppMain.js
+++ packages/nui/src/components/App/AppMain.js
@@ -2,9 +2,31 @@ import { h } from 'vue';
 
 export default {
   name: 'NAppMain',
+  data() {
+    return {
+      height: '56px',
+    };
+  },
+  mounted() {
+    this.getHeight();
+  },
+  methods: {
+    getHeight() {
+      let appBarElementHeight = 0;
+      const appBarElement = document.querySelector('.n-app-bar');
+      appBarElement &&
+        (appBarElementHeight = window.getComputedStyle(appBarElement).height);
+      this.height = appBarElementHeight;
+    },
+  },
   render() {
-    return h('div', {
-        class:['n-app-main']
-    }, this.$slots?.default());
+    return h(
+      'div',
+      {
+        class: ['n-app-main'],
+        style: `top:${this.$data.height}`,
+      },
+      this.$slots?.default()
+    );
   },
 };
