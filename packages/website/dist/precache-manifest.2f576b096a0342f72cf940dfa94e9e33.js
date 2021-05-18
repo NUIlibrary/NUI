2770a09dbf9fad0c31ec82b1cf80b8be68277bba
AllenHUSH <allenshhu@outlook.com>
2021-05-19 01:21:38 +0800

    chore: build website

---
+++ packages/website/dist/precache-manifest.2f576b096a0342f72cf940dfa94e9e33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69f392ea0ec03d27772",
+    "url": "/css/app.5fa2b8c1.css"
+  },
+  {
+    "revision": "05f43210e32f9124eab4",
+    "url": "/css/chunk-vendors.9fbeb06c.css"
+  },
+  {
+    "revision": "45fb4d0b47457c33f5419d55a756ce58",
+    "url": "/img/logo.45fb4d0b.png"
+  },
+  {
+    "revision": "fd3e01f341257c08c2dd3b4ae7e65cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69f392ea0ec03d27772",
+    "url": "/js/app.655fa3f6.js"
+  },
+  {
+    "revision": "05f43210e32f9124eab4",
+    "url": "/js/chunk-vendors.813124f9.js"
+  },
+  {
+    "revision": "07231dad28d7960adbcbe9f5e0cdfe2b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
